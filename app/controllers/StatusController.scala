21e02b11bee9c15e99851552987be782f0cdd706
pdn <pnathan@vandals.uidaho.edu>
2015-01-13 00:13:41 -0800

    Expectant API tests

--- app/controllers/StatusController.scala
+++ app/controllers/StatusController.scala
@@ -1,20 +1,34 @@
 package controllers
 
+import scala.io.Source
+
+import play.api.Play.current
 import play.api._
 import play.api.mvc._
+
 /*
  * The StatusController is an automated method to determine the health of the service.
  */
 object StatusController extends Controller {
+  // the server is healthy
   def healthz = Action {
     Ok(views.html.barebones("OK"))
   }
-  
+
+  // the server is alive
   def statusz = Action {
     Ok(views.html.barebones("Up"))
   }
-  
+
+  // versions and any other information. Should be text/json
   def propertiesz = Action {
-    Ok(views.html.barebones("1.0"))
+
+    val source = Play.current.classloader.getResource("build-data")
+    var info = "build version data unknown"
+    if (source != null) {
+      info = Source.fromURL(source).mkString.replaceAll("\\s+$", "")
+    }
+
+    Ok(views.html.barebones(info))
   }
 }
